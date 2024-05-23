99d20d9a77ed410f6b454ec17723d13f28176a29
Hichamkhalis12 <150941473+Hichamkhalis12@users.noreply.github.com>
2024-05-23 21:28:16 +0100

    Create Bo.js

---
+++ plugins/Bo.js
@@ -0,0 +1,59 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: '*قـائـمـة الـاوامــر*'
+            },
+            body: {
+              text: '🛡️ افتح القائمة بواسطة الزر\n⚡ لا تلعب كثير في القائمة'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: 'دوس هنا ',
+                    sections: [
+                      {
+                        title: 'قوائم البوت',
+                        highlight_label: 'اختار',
+                        rows: [
+                          {
+                            header: 'قسم الانمي',
+                            title: '.الانمي',
+                            description: '',
+                            id: 'te'
+                          },
+                          {
+                            header: 'قسم الاوامر',
+                            title: '.المهام',
+                            description: '',
+                            id: 'te'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
+
+}
+
+handler.menu = ['info']
+handler.hi = ['main']
+handler.hicham = ['قايمه']
+
+export default handler
+
+
+`ملحوظة الكود منقول من قناة ميجو` 
+
+> ROPERTO🇪🇬
